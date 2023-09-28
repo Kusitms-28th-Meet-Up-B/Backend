5393ed489b17cde195b02b6394b57b4bdf86ecf7
MyunghyunNero <mh1kim0910@gmail.com>
2023-09-28 13:22:29 +0900

    [init] initialize

---
+++ src/main/java/kusitms/gallae/GallaeApplication.java
@@ -0,0 +1,13 @@
+package kusitms.gallae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kusitms/gallae/GallaeApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.gallae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
