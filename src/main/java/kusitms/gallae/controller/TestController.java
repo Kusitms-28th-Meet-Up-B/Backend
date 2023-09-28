313b8e7c9ea337a37697161a1247ae867601d758
MyunghyunNero <mh1kim0910@gmail.com>
2023-09-28 13:38:24 +0900

    [feat] Add TestController

---
+++ src/main/java/kusitms/gallae/controller/TestController.java
@@ -0,0 +1,14 @@
+package kusitms.gallae.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String basicTest(){
+        return "test";
+    }
+}
