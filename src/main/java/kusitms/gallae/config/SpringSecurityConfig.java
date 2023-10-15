b8a610cb9cab1453cd9eb0b45fcc57e540fefa89
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-16 00:57:06 +0900

    feat : security config에 예외 핸들러 추가

--- src/main/java/kusitms/gallae/config/SpringSecurityConfig.java
+++ src/main/java/kusitms/gallae/config/SpringSecurityConfig.java
@@ -1,23 +1,36 @@
 package kusitms.gallae.config;
 
 
+import kusitms.gallae.global.jwt.JwtAccessDeniedHandler;
+import kusitms.gallae.global.jwt.JwtAuthenticationEntryPoint;
+import kusitms.gallae.global.jwt.JwtAuthenticationFilter;
+import kusitms.gallae.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig {
+    private final JwtProvider jwtAuthenticationProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll().and()
+                .exceptionHandling()
+                .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(this.jwtAccessDeniedHandler).and()
+                .addFilterBefore(new JwtAuthenticationFilter(this.jwtAuthenticationProvider), UsernamePasswordAuthenticationFilter.class);
+
 
         return httpSecurity.build();
     }
