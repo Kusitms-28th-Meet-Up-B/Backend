6adc96dfae19bf71cddcafd3c430806964bd3782
MyunghyunNero <mh1kim0910@gmail.com>
2023-10-16 00:28:09 +0900

    feat : user 추가

--- src/main/java/kusitms/gallae/domain/User.java
+++ src/main/java/kusitms/gallae/domain/User.java
@@ -1,54 +1,56 @@
-//package kusitms.gallae.domain;
-//
-//
-//import jakarta.persistence.*;
-//import kusitms.gallae.global.Role;
-//import lombok.Builder;
-//import lombok.ToString;
-//import org.apache.commons.lang3.RandomStringUtils;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "user")
-//@Builder
-//@ToString
-//public class User {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(length = 45, nullable = false)
-//    private String username;
-//
-//    @Column(length = 45, nullable = false)
-//    private String loginId;
-//
-//    @Column(length = 500, nullable = false)
-//    private String passward;
-//
-//    @Column(length = 500, nullable = false)
-//    private String refreshToken;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false, columnDefinition = "ENUM")
-//    private Role role;
-//
-//    @CreationTimestamp
-//    @Column
-//    private LocalDateTime createdAt;
-//
-//    @UpdateTimestamp
-//    @Column
-//    private LocalDateTime updatedAt;
-//
-//    @Column
-//    private LocalDateTime deletedAt;
-//
-//    public void renewRefreshToken() {
-//        this.refreshToken = RandomStringUtils.randomAlphanumeric(32);
-//    }
-//}
+package kusitms.gallae.domain;
+
+
+import jakarta.persistence.*;
+import kusitms.gallae.global.Role;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@RequiredArgsConstructor
+@Builder
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String username;
+
+    @Column(length = 45, nullable = false)
+    private String loginId;
+
+    @Column(length = 500, nullable = false)
+    private String passward;
+
+    @Column(length = 500, nullable = false)
+    private String refreshToken;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "ENUM")
+    private Role role;
+
+    @CreationTimestamp
+    @Column
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public void renewRefreshToken() {
+        this.refreshToken = RandomStringUtils.randomAlphanumeric(32);
+    }
+}
