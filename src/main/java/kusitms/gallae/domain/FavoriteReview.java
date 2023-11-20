e40ef16510210260b6de1f3710b93db8f1c2a4a7
MyunghyunNero <mh1kim0910@gmail.com>
2023-11-20 19:47:50 +0900

    feat : 리뷰 ,자료실 좋아요 테이블 생성

---
+++ src/main/java/kusitms/gallae/domain/FavoriteReview.java
@@ -0,0 +1,30 @@
+package kusitms.gallae.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "favoriteReview")
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@ToString
+public class FavoriteReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_Id", nullable = false)
+    @ToString.Exclude
+    private User user;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "review_Id", nullable = false)
+    @ToString.Exclude
+    private Review review;
+}
